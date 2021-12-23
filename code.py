84895947c3f16fcf1ff021591f90076142944621
Stephane Camberlin <sca@enotty.io>
2021-12-23 14:46:06 +0100

    fix scd

--- code.py
+++ code.py
@@ -58,11 +58,26 @@ except Exception as e:
 
 w.feed()
 
-i2c = board.I2C()
-#i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
+#i2c = board.I2C()
+i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
 scd = adafruit_scd30.SCD30(i2c)
 scd.self_calibration_enabled = True
 scd.forced_recalibration_reference = 412
+
+# The SCD30 reset generates a hiccup on the SCL and SDA lines
+# which can end up not being handled well by different hosts.
+scd.reset()
+
+# The MCP2221 is known to not like the SCD30 reset hiccup.
+# See below for more information:
+# https://github.com/adafruit/Adafruit_CircuitPython_SCD30/issues/2
+# Can get around it by resetting via this hack.
+# pylint:disable=protected-access
+if hasattr(i2c, "_i2c"):
+  # we're using Blinka, check for MCP2221
+  if hasattr(i2c._i2c, "_mcp2221"):
+    # reset it
+    i2c._i2c._mcp2221._reset()
 
 w.feed()
 
